8433e717d2fcc9760f3d08d6974d99228c36a00f
sakuja <shusaku.k@gmail.com>
2025-03-07 19:43:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log('Updating table with data');
 
         // タイトルを省略する関数を追加
-　　function truncateTitle(title, maxLength = 30) {
+　　function truncateTitle(title, maxLength = 80) {
     if (!title) return 'No Title';
     if (title.length <= maxLength) return title;
     return title.substring(0, maxLength) + '...';
